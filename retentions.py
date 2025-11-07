395555babcadc0d3415ee0edcae2c40946e8f41d
Thomas Kuhlmann <thomas.kuhlmann@thk-systems.de>
2025-11-07 20:42:37 +0100

    Added empty argparser

--- retentions.py
+++ retentions.py
@@ -1,6 +1,16 @@
-def test() -> None:
-    print("test")
+import argparse
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    # TODO - Define parser
+    return parser.parse_args()
+
+
+def main() -> None:
+    arguments = parse_arguments()
+    print(arguments)
 
 
 if __name__ == "__main__":
-    test()
+    main()
