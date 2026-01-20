94b4fe082a71e7049fad977b8bf4abde1b24150b
Thomas Kuhlmann <thomas.kuhlmann@thk-systems.de>
2026-01-20 14:24:29 +0100

    Supplemented `FileStats.get_file_bytes()` with `folder_mode`

--- retentions.py
+++ retentions.py
@@ -69,6 +69,8 @@ class FileStats:
         return int(getattr(self.__file_stats_cache.setdefault(file, file.stat()), f"st_{self._age_type}"))
 
     def get_file_bytes(self, file: Path) -> int:
+        if self._folder_mode:
+            return sum(self.__file_stats_cache.setdefault(f, f.stat()).st_size for f in file.rglob("*") if f.is_file() and not f.is_symlink())
         return self.__file_stats_cache.setdefault(file, file.stat()).st_size
 
 
